e6fe2bc9fbe7750adcf50ee631806b223c994247
francislin <francislindosreis@hotmail.com>
2026-05-08 01:16:19 -0300

    Change the ExceptionHandle

--- src/main/java/com/ebanx/challenge/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ebanx/challenge/exception/GlobalExceptionHandler.java
@@ -1,22 +1,21 @@
 package com.ebanx.challenge.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import java.util.Map;
 import java.util.NoSuchElementException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<?> handleNotFound(NoSuchElementException e) {
-        return ResponseEntity.status(404).body(Map.of("error", e.getMessage()));
+    public ResponseEntity<Integer> handleNotFound(NoSuchElementException e) {
+        return new ResponseEntity<>(0, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<?> handleBadRequest(IllegalArgumentException e) {
-        return ResponseEntity.status(422).body(Map.of("error", e.getMessage()));
+    public ResponseEntity<Integer> handleBadRequest(IllegalArgumentException e) {
+        return new ResponseEntity<>(0, HttpStatus.NOT_FOUND);
     }
 }
